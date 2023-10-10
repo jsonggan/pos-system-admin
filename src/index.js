7abcc48f5a19dc990a5947f8aee40a7e897d5067
Chin Song <songganjs@gmail.com>
2023-10-10 18:08:06 +0800

    clean up

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { createBrowserHistory } from 'history';
-
-const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
